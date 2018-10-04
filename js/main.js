74870a44c93a810988d30e914b2c3550000a7c74
unknown <j_cho6@fanshaweonline.ca>
2018-10-04 01:30:28 -0400

    object, javascript finished

--- js/main.js
+++ js/main.js
@@ -1,98 +1,11 @@
- //variables
- let clickImg = document.querySelectorAll(".clickme"),
-     imageBox = document.querySelector("#imgBox"),
-     close = document.querySelector("#close"),
-     lightBox = document.querySelector("#lightbox"),
-     nav = document.querySelector("#mainNav"),
-     name = document.querySelector("#name"),
-     desc = document.querySelector("#desc"),
-     leftBtn = document.querySelector("#left"),
-     rightBtn = document.querySelector("#right");
-     
- const arrName = ["Cho","Andrino"];
- const arrText = ["Kim Hanbok Project","First Year Integrated Project","Sci-Fi Fan","The Dark Mine","Small World"];
- const imgArr = ["cho1_l","cho2_l","andrino1_l","andrino2_l","andrino3_l"];
- let idx=0;
+const memberInfo = document.querySelector('#aboutUs').querySelectorAll('p');
 
-//functions
-//imageSwap changes image in lightbox
- imageSwap=(e)=>{
-   lightBox.className = "open";
-  nav.style.visibility='hidden';
-  imageBox.src = e.target.src;
-  nameSwap();
-  textSwap();
- };
-
-
-  nameSwap=()=>{
-    if (imageBox.src.match("cho")){
-      idx=0;
-    }else{
-      idx=1;
-    }
-    name.innerText=arrName[idx];
-  }
-  textSwap=()=>{
-    if (imageBox.src.match("cho1")){
-      idx=0;
-    }
-    if (imageBox.src.match("cho2")){
-      idx=1;
-    }
-    if (imageBox.src.match("andrino1")){
-      idx=2;
-    }
-    if (imageBox.src.match("andrino2")){
-      idx=3;
-    }
-    if (imageBox.src.match("andrino3")){
-      idx=4;
-    }
-    
-    desc.innerText=arrText[idx];  
-}
-//button function change Pic
-  next=()=>{
-  idx++;
-  if(idx==5)idx=0;
-  imageBox.src = "images/"+imgArr[idx]+".png";
-  nameSwap();
-  textSwap();
-}
-
-prev=()=>{
-  idx--;
-  if(idx==-1)idx=4;
-  imageBox.src = "images/"+imgArr[idx]+".png";
-  nameSwap();
-  textSwap();
+function setMemberInfo(data){
+    memberInfo[0].textContent = `Hello! My name is ${data.membername[0]} - ${data.memberrole[0]} of the Site`;
+    memberInfo[1].textContent = data.memberwords[0];
+    memberInfo[2].textContent = `Hello! My name is ${data.membername[1]} - ${data.memberrole[1]} of the Site`;
+    memberInfo[3].textContent = data.memberwords[1];
 }
 
 
- closeBtn=()=>{
-   lightBox.className = "";
-   nav.style.visibility='visible';
- };
-
- closeClick=(e)=>{
-  // console.log(e.target);
-if (e.target.id == "lightbox_content") {
-lightBox.className = "";
-nav.style.visibility='visible';
-}
-};
-
-//eventListeners
-for(let i=0; i<clickImg.length; i++){
- clickImg[i].addEventListener("click",imageSwap,false);
-}
-close.addEventListener("click",closeBtn,false);
-lightBox.addEventListener("click",closeClick,false);
-leftBtn.addEventListener("click",prev,false);
-rightBtn.addEventListener("click",next,false);
-
-//--------------------------------------------------------
-
-
-
+setMemberInfo(memberData);
